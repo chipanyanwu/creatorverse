3d7587934b9583d3a2858cd11cd16cbb4528c1e1
chip <49256163+chipanyanwu@users.noreply.github.com>
2023-08-15 23:35:24 -0400

    finished designing creator card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,20 @@
+import { Link } from "react-router-dom"
+
+import EditIcon from "../assets/edit.svg"
+import InstagramIcom from "../assets/instagram.svg"
+import TwitterIcom from "../assets/twitter.svg"
+import YoutubeIcom from "../assets/youtube.svg"
+import TikTokIcon from "../assets/tiktok.svg"
+import TwitchIcon from "../assets/twitch.svg"
+
 import "./Card.css"
 
 export default function Card() {
   return (
     <div className="card">
-        <button className="card-edit-btn">E</button>
+        <button className="card-edit-btn">
+            <img src={EditIcon}/>
+        </button>
         <img className="card-img" src="https://e00-marca.uecdn.es/assets/multimedia/imagenes/2023/08/07/16914411203676.jpg"/>
         <div className="card-content">
             <div className="card-header">
@@ -11,15 +22,25 @@ export default function Card() {
                     <span className="fancy">Kai Cenat</span>
                 </h2>
                 <div className="card-socials">
-                    <p>IG</p>
-                    <p>X</p>
-                    <p>YT</p>
-                    <p>TT</p>
-                    <p>TW</p>
+                    <Link to="https://www.instagram.com/" target="_blank">
+                        <img src={InstagramIcom}/>
+                    </Link>
+                    <Link to="https://www.instagram.com/" target="_blank">
+                        <img src={TwitterIcom}/>
+                    </Link>
+                    <Link to="https://www.instagram.com/" target="_blank">
+                        <img src={YoutubeIcom}/>
+                    </Link>
+                    <Link to="https://www.instagram.com/" target="_blank">
+                        <img src={TikTokIcon}/>
+                    </Link>
+                    <Link to="https://www.instagram.com/" target="_blank">
+                        <img src={TwitchIcon}/>
+                    </Link>
                 </div>
             </div>
             <div className="card-desc">
-                <p>Lorem ipsum dolor sit amet, consectetur</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam voluptatem sunt voluptates optio minima. Excepturi fugiat voluptatibus rerum eaque, quod, corporis sunt deserunt ex dolor doloremque explicabo nesciunt, laboriosam voluptatum.</p>
             </div>
         </div>
     </div>
